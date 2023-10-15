0ac5c868acd16684573eaa281ee01958bf144e10
sumanpanda27 <sumanpanda27@gmail.com>
2023-10-15 22:36:43 +0530

    flashcard generator completed

---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Logo from "./logo.png";
+
+const Navbar = () => {
+  return (
+    <div className="h-14 w-screen fixed z-50 bg-white dark:bg-[#1e2125] flex items-center space-between drop-shadow-md">
+      <div className="flex items-center font-medium text-3xl">
+        <img className="h-10 my-auto ml-4" src={Logo} alt="AlmaBetter" />
+        <span className="dark:text-white">maBetter</span>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
